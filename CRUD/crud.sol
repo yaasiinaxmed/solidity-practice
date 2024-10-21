627aafe363810dd66d4bd2cdb8dbcd3bec889646
yaasiinaxmed <yaskassoy@gmail.com>
2024-10-21 13:03:54 +0300

    create new item

--- CRUD/crud.sol
+++ CRUD/crud.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract CRUD {
+    uint64 public itemCount = 0
+
+    struct Item {
+        uint id;
+        string description;
+    }
+
+    mapping(uint => Item) public items
+
+    // Create a new item
+    function createItem(string memory _description) public {
+        itemCount++
+        items[itemCount] = Item(itemCount, _description);
+    }
+    
+}
